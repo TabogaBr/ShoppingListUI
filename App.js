c4c0749e14ac4fdfcd442d508ecd1b43e77b81ad
TabogaBr <brenda.taboga7@gmail.com>
2022-10-22 14:14:09 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { Input, Header, Button, ListItem } from 'react-native-elements';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [list, setList] = useState([]);
+
+  const renderItem = ({ item }) => (
+      <ListItem.Swipeable
+        rightContent={
+          <Button
+            title="Delete"
+            icon={{ name: 'delete', color: 'white' }}
+            buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
+            onPress={() => deleteItem(item)}
+          />
+        }>
+        <ListItem.Content>
+          <ListItem.Title>{item.product}</ListItem.Title>
+          <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem.Swipeable>
+  );
+
+
+  const saveItem = () => {
+    setList([...list, { product: product, amount: amount }]);
+    setProduct('');
+    setAmount('');
+  }
+
+  const deleteItem = (item) => {
+    setList(list.filter((i) => i !== item));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+        placement='center'
+        centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff' } }} />
+      <Input
+        placeholder='Product' label='PRODUCT'
+        onChangeText={product => setProduct(product)}
+        value={product} />
+      <Input
+        placeholder='Amount' label='AMOUNT'
+        onChangeText={amount => setAmount(amount)}
+        value={amount} />
+      <Button
+        style={styles.button}
+        raised icon={{ name: 'save' }} onPress={saveItem} title='SAVE' />
+      <View style={styles.list}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          ListHeaderComponent={<Text style={styles.title} > Shopping List </Text>}
+        />
+      </View>
     </View>
   );
 }
@@ -16,5 +70,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    width: 200,
+  },
+  list: {
+    width: '80%',
+    flex: 1,
+    backgroundColor: 'white',
+    marginTop: 20,
+  },
+  title: {
+    color: 'blue',
+    fontSize: 18,
   },
 });
